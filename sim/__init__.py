c02f2dc51ecc3e06ab71ea09bfde91e57ab5e1f5
Maksim Shabunov <maksim.shabunov@gmail.com>
2026-08-06 10:23:47 +0300

    Add simulation package initializer

---
+++ sim/__init__.py
@@ -0,0 +1,5 @@
+"""Downlink window simulator. Mostly a stub -- see sim/window.py."""
+
+from .window import DownlinkWindow, chronological_order, plan_windows, replay
+
+__all__ = ["DownlinkWindow", "chronological_order", "plan_windows", "replay"]
